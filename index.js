eb835b13b509f500645f33d07bd16db4e2af1fb9
Sanu Kumar <sanukumar@Sanus-MacBook-Air.local>
2021-07-26 10:00:20 +0530

    added uuid v4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express();
 const server=require("http").Server(app);
-
+const {v4:uuidv4}=require("uuid");
 
 app.use(express.static('public'));
 
@@ -12,7 +12,13 @@ app.set('view engine','ejs');
 
 app.get("/",function(req,res){
     console.log("server is runing");
-    res.render('room');
-})
+    // res.render('room');
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room",function(req,res)
+{
+    res.render("room",{roomId:req.param.room});
+});
 
 server.listen(2000);
